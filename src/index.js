b1c526f7a670ac846bd283b42405e65f7d2d19ee
Aduda-Boaz <adudaboaz@gmail.com>
2022-05-17 20:36:12 +0300

    Modify the react app to the newer version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root'),
+    </Provider>
+  </React.StrictMode>,
 );
